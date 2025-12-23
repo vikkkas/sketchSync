99303006568abdabf4f9cdc7df57e47570aafa32
Vikas Prasad <vikas.prasad@globalgarner.com>
2025-12-23 18:30:34 +0530

    cors fix

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
